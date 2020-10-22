f821a545c360dc2ddb4534a17e48afc4bb735cc7
skibitsky <gleb@skibitsky.com>
2020-10-22 11:48:20 +0300

    Project created

---
+++ Assets/Scripts/MyScript.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.Localization;
+
+public class MyScript : MonoBehaviour
+{
+    // Player's name to be used as an argument in _myString
+    public string PlayerName => "Neo";
+    
+    [SerializeField] private LocalizedString _myString;
+}
